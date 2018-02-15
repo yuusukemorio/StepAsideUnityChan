1940479d19d1667e4fde39dd2c483ee71bd199ee
yuusukemorio <Morimori2003@gmail.com>
2018-02-15 15:15:06 +0900

    StepAsideUnityChanの課題です　 よろしくお願いします

---
+++ Assets/Delete.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delete : MonoBehaviour
+{
+
+    private GameObject unitychan;
+    // Use this for initialization
+    void Start()
+    {
+        unitychan = GameObject.Find("unitychan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameObject.transform.position.z < unitychan.transform.position.z - 10)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
